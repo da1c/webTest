94c536ac9c3305f789ee27bd9d0576140375abe5
da1c <axda1c21@gmail.com>
2020-05-29 13:33:44 +0900

    端末を識別してmenuheader.cssを読み込むように修正

---
+++ js/myjs/selectCSS.js
@@ -0,0 +1,18 @@
+
+
+
+/**
+ *メニュー部分のCSS読み込み
+ *
+ */
+function SelectMenuHaderCSS(){
+
+    let userArgent = navigator.userAgent;
+    if (userArgent.indexOf('iPhone') > -1 || userArgent.indexOf('iPod') > -1) {
+        document.write('<link rel="stylesheet" href="./../css/common/menuheader/iphone.css?DATE=20200529a">');
+    }else if(userArgent.indexOf('Android') > -1){
+        document.write('<link rel="stylesheet" href="./../css/common/menuheader/android.css?DATE=20200529a">');
+    }else{
+        document.write('<link rel="stylesheet" href="./../css/common/menuheader/iphone.css?DATE=20200529a">');
+    }
+}
