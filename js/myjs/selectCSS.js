ba34a7134967db14f076a40cbd59e437846a71f2
da1c <axda1c21@gmail.com>
2020-05-29 14:04:38 +0900

    selectCSS.jsの読み込み時のバージョンアップ漏れ

--- js/myjs/selectCSS.js
+++ js/myjs/selectCSS.js
@@ -11,7 +11,7 @@ function SelectMenuHaderCSS(){
     if (userArgent.indexOf('iPhone') > -1 || userArgent.indexOf('iPod') > -1) {
         document.write('<link rel="stylesheet" href="./../css/common/menuheader/iphone.css?DATE=20200529a">');
     }else if(userArgent.indexOf('Android') > -1){
-        document.write('<link rel="stylesheet" href="./../css/common/menuheader/android.css?DATE=20200529b">');
+        document.write('<link rel="stylesheet" href="./../css/common/menuheader/android.css?DATE=20200529c">');
     }else{
         document.write('<link rel="stylesheet" href="./../css/common/menuheader/iphone.css?DATE=20200529a">');
     }
