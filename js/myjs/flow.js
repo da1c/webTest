f6aea7ee044ca2dc49fb1a5104ccd940c27010cb
da1c <axda1c21@gmail.com>
2020-06-02 17:28:50 +0900

    itemPickUp.htmlをIframeからindex.htmlに移植

--- js/myjs/flow.js
+++ js/myjs/flow.js
@@ -1,6 +1,6 @@
 "use strict";
 
-function ModelViewRender(){
+function ModelViewRender() {
   requestAnimationFrame(ModelViewRender);
   Flow.modelView.Render();
 }
@@ -14,57 +14,52 @@ class flow {
     this.indexWnd = null;
     this.menuWnd = null;
     this.menuAreaHeight = 0.0;
-
     this.modelView = null;
+
+    this.MenuStateID = {
+      CATEGORY: 0,
+      ITEM_PICKUP: 1,
+      COLOR_TYPE: 2,
+      COLOR_PICKUP: 3,
+    };
+    this.nowMenuStateID = this.MenuStateID.CATEGORY;
   }
 
-  Init(){
+  Init() {
     this.modelView = new ModelView();
-    this.modelView.Init( window.innerWidth, this.GetScreenHarfSize() );
+    this.modelView.Init(window.innerWidth, this.GetScreenHarfSize());
 
     ModelViewRender();
+
+    // 商材名設定
+    let itemName = window.top.dataMng.GetNowItemName()
+    this.UpdateItemName(itemName);
   }
 
   SetIndexWindow(wnd) {
     this.indexWnd = wnd;
   }
 
-  SetMenuWindow(wnd) {
-    this.menuWnd = wnd;
-  }
-
-  SetModelViewWindow(wnd) {
-    this.modelWnd = wnd;
-  }
-
   // 遷移などをここに実装
   ClickItemCategory() {
-    this.GetMenuWindow().location.href = "./../itemPickUp/itemPickUp.html";
+    this.ChangeState( this.MenuStateID.ITEM_PICKUP );
   }
 
-  ClickClose(){
+  ClickClose() {
     this.indexWnd.$(".itemSelectModal").fadeOut();
   }
 
-  ClickCloseVideo(){
+  ClickCloseVideo() {
     this.indexWnd.$(".videoModal").fadeOut();
   }
 
-  GetMenuWindow() {
-    return this.indexWnd.frames.menu;
-  }
-
-  GetModelViewWindow() {
-    return this.indexWnd.frames.modelView;
-  }
-
-  GetMenuAreaHeight(){
+  GetMenuAreaHeight() {
     return this.menuAreaHeight;
   }
 
   // ARボタンクリック時の処理
-  ClickARButton(){
-    let arAddress  = this.indexWnd.dataMng.GetSelectItemARUrl();
+  ClickARButton() {
+    let arAddress = this.indexWnd.dataMng.GetSelectItemARUrl();
     this.indexWnd.open(arAddress);
   }
 
@@ -78,22 +73,20 @@ class flow {
   }
 
   // ボタンクリック
-  ClickMenuButton(){
+  ClickMenuButton() {
     // メニュー取得
     let menu = this.indexWnd.$(".menuModal");
     let headerHeight = this.indexWnd.$(".Header").innerHeight();
 
-    menu.css("top", headerHeight + "px" );
-    // 
+    menu.css("top", headerHeight + "px");
+    //
     let content = this.indexWnd.$(".modal_menu");
-    content.css( { top : "0px", 
-                   left : "0px",
-                   transformX : "translate(-50%)"  } );
+    content.css({ top: "0px", left: "0px", transformX: "translate(-50%)" });
 
     menu.fadeIn();
   }
-  
-  ClickCloseMenu(){
+
+  ClickCloseMenu() {
     this.indexWnd.$(".menuModal").fadeOut();
   }
 
@@ -108,51 +101,65 @@ class flow {
     // モデルビューのウィンドウ取得
     this.indexWnd.dataMng.nowItemID = id;
     // モデルウィンドウのモデル更新
-    this.modelView.SetModel( this.indexWnd.dataMng.GetNowModelPath(), this.indexWnd.dataMng.GetNowModelWallPos(), this.indexWnd.dataMng.GetNowModelFloorPos() );
+    this.modelView.SetModel(
+      this.indexWnd.dataMng.GetNowModelPath(),
+      this.indexWnd.dataMng.GetNowModelWallPos(),
+      this.indexWnd.dataMng.GetNowModelFloorPos()
+    );
 
     // 平行光源の強さ設定
     let intensity = this.indexWnd.dataMng.GetDirLightIntensity();
     this.modelView.SetDirLightIntensity(intensity);
 
-    // menuエリアの表示をカテゴリー選択画面に設定
-    this.GetMenuWindow().location.href = "./menu/menu.html";
+    // ステートをカテゴリーに戻す
+    this.ChangeState(this.MenuStateID.CATEGORY);
+    // 商材名更新
+    let itemName = window.dataMng.GetNowItemName()
+    this.UpdateItemName(itemName);
+
     // フェードアウトさせる
     this.indexWnd.$(".itemSelectModal").fadeOut();
   }
 
 
   /**
+   *商材名表記更新
+   * @param {*} itemName
+   * @memberof flow
+   */
+  UpdateItemName(itemName){
+    window.$(".HeaderItemName").html(itemName);
+  }
+
+  /**
    *小組一覧表示ボタンクリック時の処理
    *
    * @memberof flow
    */
-  ClickItemDetailListButton(){
-
+  ClickItemDetailListButton() {
     // 現在選択中商材の小組情報一覧を取得
     let detailInfo = this.indexWnd.dataMng.GetNowItemDetailInfo();
 
     // 画像を差し替える 小組名も設定
-    this.indexWnd.$(".DetailBanner").each(function(){
-      // 
+    this.indexWnd.$(".DetailBanner").each(function () {
+      //
       let idx = $(this).index();
 
       // アイコンの要素に画像パスを設定
       let icon = $(this).children(".icon");
       let iconImg = icon.children(".iconimg");
-      iconImg.attr( "src", detailInfo[idx].SRC );
+      iconImg.attr("src", detailInfo[idx].SRC);
 
       // 小組名設定
       let nameArea = $(this).children(".ItemNameArea");
       let name = nameArea.children(".ItemName");
-      name.html( detailInfo[idx].NAME );
-
+      name.html(detailInfo[idx].NAME);
     });
 
     this.indexWnd.$(".itemDetailModal").fadeIn();
   }
 
-
-  ClickDetailButton(idx){
+  ClickDetailButton(idx) {
     console.log(idx);
     // 選択したIDXを保存
     this.indexWnd.dataMng.selectDetailID = idx;
@@ -161,8 +168,7 @@ class flow {
     this.GetMenuWindow().location.href = "./../itemDetail/itemDetail.html";
   }
 
-  ClickDetailListButton(idx){
-
+  ClickDetailListButton(idx) {
     // フェードアウトさせる
     this.indexWnd.$(".itemDetailModal").fadeOut();
 
@@ -173,12 +179,12 @@ class flow {
     this.GetMenuWindow().location.href = "./itemDetail/itemDetail.html";
   }
 
-  ClickCloseItemDetailListButton(){
+  ClickCloseItemDetailListButton() {
     // フェードアウトさせる
     this.indexWnd.$(".itemDetailModal").fadeOut();
   }
 
-  Resize(){
+  Resize() {
     let screenHegiht = $(window).innerHeight();
     let screenHarfHegiht = this.GetScreenHarfSize();
 
@@ -190,70 +196,96 @@ class flow {
     this.menuAreaHeight = screenHarfHegiht;
   }
 
-  GetScreenHarfSize(){
+  GetScreenHarfSize() {
     let screenHegiht = $(window).innerHeight();
     return screenHegiht * 0.47;
   }
 
   // カテゴリーのカラーを選択
-　ClickColorCategory(){
+  ClickColorCategory() {
     // カラーカテゴリー選択
     let menuWnd = this.GetMenuWindow();
 
     menuWnd.location.href = "./../colorTypeSelect/colorSelectType.html";
   }
 
-
   /**
    *機能選択初期化
    * @memberof flow
    */
-  InitItemPickUp(){
+  InitItemPickUp() {
     // メニュー部分のwindowオブジェクトの取得
-    let wnd = this.GetMenuWindow();
+    let wnd = window;
 
     // 機能情報を取得
     let itemInfo = this.indexWnd.dataMng.GetNowItemDetailInfo();
     let element_str = "";
 
     for (let index = 0; index < itemInfo.length; index++) {
-
-      let imgType ="";
+      let imgType = "";
 
       // 画像のサイズ確認
-      if( this.indexWnd.dataMng.CheckDispSize(itemInfo[index].DISP_SIZE))
-      {
+      if (this.indexWnd.dataMng.CheckDispSize(itemInfo[index].DISP_SIZE)) {
         // 通常サイズ
         imgType = "SlickElementImg";
-      }else{
+      } else {
         // 横長サイズ
         imgType = "SlickElementWideImg";
       }
 
       // タイプの確認
-      if( this.indexWnd.dataMng.CheckIMGSrcType(itemInfo[index].TYPE)){
+      if (this.indexWnd.dataMng.CheckIMGSrcType(itemInfo[index].TYPE)) {
         // 通常
-        element_str += this.CreateIMGElement( itemInfo[index].SRC, imgType );
-      }
-      else{
+        element_str += this.CreateIMGElement(itemInfo[index].SRC, imgType);
+      } else {
         // 動画リンクの場合
-        element_str += this.CreateVideoElement(itemInfo[index].SRC, imgType, itemInfo[index].NAME);
+        element_str += this.CreateVideoElement(
+          itemInfo[index].SRC,
+          imgType,
+          itemInfo[index].NAME
+        );
       }
     }
 
     // 追加先の要素を取得
-    let dstElement = wnd.$(".slick-box");
+    let dstElement = wnd.$(".MenuParent");
     dstElement.append(element_str);
+
+    dstElement.slick({
+      centerMode: true,
+      centerPadding: "15%",
+      dots: false,
+      infinite: false,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      speed: 300,
+      adaptiveHeight: true,
+      arrows: false,
+      variableWidth: true,
+    });
   }
 
-  CreateIMGElement(src, imgType){
-    return "<li><div class=\"SlickElement\"><img class=\""+ imgType+ " \" src=\"../" + src + "\"></div></li>";
+  CreateIMGElement(src, imgType) {
+    return (
+      '<li><div class="SlickElement"><img class="' +
+      imgType +
+      ' " src="' +
+      src +
+      '"></div></li>'
+    );
   }
 
-  CreateVideoElement(src, imgType, videoID){
-    return "<li><div class=\"SlickElement\"><img class=\""+ imgType + "\" src=\"../" + src + "\" onclick=\"window.top.Flow.ClickVideo(" + videoID + ")\"><img class=\"MovieIcon\" src=\"../img/movie_icon.png\"></div></li>";
+  CreateVideoElement(src, imgType, videoID) {
+    return (
+      '<li><div class="SlickElement"><img class="' +
+      imgType +
+      '" src="' +
+      src +
+      '" onclick="window.top.Flow.ClickVideo(' +
+      videoID +
+      ')"><img class="MovieIcon" src="./img/movie_icon.png"></div></li>'
+    );
   }
-
 
   /**
    *colorPickUpの初期化処理
@@ -261,7 +293,7 @@ class flow {
    *
    * @memberof flow
    */
-  InitColorPickUp(){
+  InitColorPickUp() {
     // カラー一覧の初期化
     let wnd = this.GetMenuWindow();
 
@@ -270,7 +302,7 @@ class flow {
 
     // パンくずに選択したカラー箇所名を設定
     let bread = wnd.$(".breadcrumb_current");
-    bread.html( colorInfo.COLOR_CATEGORY );
+    bread.html(colorInfo.COLOR_CATEGORY);
 
     // 追加先の要素を取得
     let dstElement = wnd.$(".slick-box");
@@ -278,20 +310,25 @@ class flow {
 
     // カラー情報分、スクロール要素配下に要素を追加
     for (let index = 0; index < colorInfo.COLOR_ID_ARRAY.length; ++index) {
-      const element = this.indexWnd.dataMng.GetColorInfo( colorInfo.COLOR_ID_ARRAY[index]);
-      element_str += "<li class=\"SlickElement ContentsParent\"><div class=\"ColorName\">"+ element.NAME +"</div><img class=\"SlickElementImg ContentsChild\" src=\"../" + element.SRC + "\" /></li>";
+      const element = this.indexWnd.dataMng.GetColorInfo(
+        colorInfo.COLOR_ID_ARRAY[index]
+      );
+      element_str +=
+        '<li class="SlickElement ContentsParent"><div class="ColorName">' +
+        element.NAME +
+        '</div><img class="SlickElementImg ContentsChild" src="' +
+        element.SRC +
+        '" /></li>';
     }
 
-    dstElement.append( element_str );
-
+    dstElement.append(element_str);
   }
-
 
   /**
    *カラー選択画面初期化
    * @memberof flow
    */
-  InitColorTypeSelect(){
+  InitColorTypeSelect() {
     // Menu部分のウィンドウ取得
     let wnd = this.GetMenuWindow();
 
@@ -305,13 +342,16 @@ class flow {
     // カラー情報分、スクロール要素配下に要素を追加
     for (let index = 0; index < colorInfo.length; ++index) {
       const element = colorInfo[index];
-      element_str += "<div class=\"CategoryBanner\" onclick=\"window.top.Flow.ClickColorType(" + index + ")\"><div class=\"CategoryGuide\"><div class=\"CategoryBannerName\">" + element.COLOR_CATEGORY +"</div></div><div class=\"CategoryBannerMark\">></div></div>";
+      element_str +=
+        '<div class="CategoryBanner" onclick="window.top.Flow.ClickColorType(' +
+        index +
+        ')"><div class="CategoryGuide"><div class="CategoryBannerName">' +
+        element.COLOR_CATEGORY +
+        '</div></div><div class="CategoryBannerMark">></div></div>';
     }
 
-    dstElement.append( element_str );
-
+    dstElement.append(element_str);
   }
-
 
   /**
    *カラータイプクリック
@@ -319,8 +359,7 @@ class flow {
    * @param {*} ID
    * @memberof flow
    */
-  ClickColorType(ID){
-
+  ClickColorType(ID) {
     this.indexWnd.dataMng.SetSelectColorType(ID);
 
     // カラーカテゴリー選択
@@ -329,9 +368,8 @@ class flow {
     menuWnd.location.href = "./../colorPickUp/colorPickUp.html";
   }
 
-
   // 機能の動画を選択
-  ClickVideo(ID){
+  ClickVideo(ID) {
     let info = this.indexWnd.dataMng.GetVideoInfo(ID);
     this.indexWnd.open(info.SRC);
 
@@ -339,18 +377,18 @@ class flow {
     this.VideoViewFadeIn();
   }
 
-  SetVideoView(src){
+  SetVideoView(src) {
     this.indexWnd.$(".VideoView").attr("src", src);
   }
-  VideoViewFadeIn(){
+  VideoViewFadeIn() {
     this.indexWnd.$(".VideoArea").fadeIn();
   }
 
-  VideoViewFideOut(){
+  VideoViewFideOut() {
     this.indexWnd.$(".VideoArea").fadeOut();
   }
 
-  SetVideoNameText(text){
+  SetVideoNameText(text) {
     this.indexWnd.$(".VideoName").text(text);
   }
 
@@ -359,29 +397,27 @@ class flow {
    *
    * @memberof flow
    */
-  ClickCloseVideo(){
+  ClickCloseVideo() {
     this.VideoViewFideOut();
     this.SetVideoView("");
   }
 
-  
   /**
    *Webカタログバナーをクリックした際の処理
    *
    * @memberof flow
    */
-  ClickWebCatalogCategory(){
+  ClickWebCatalogCategory() {
     let url = this.indexWnd.dataMng.GetWebCataroguURL();
     this.indexWnd.open(url);
   }
-
 
   /**
    *ホームページボタンクリック
    *
    * @memberof flow
    */
-  ClickHomePage(){
+  ClickHomePage() {
     let url = this.indexWnd.dataMng.GetHomePageURL();
     this.indexWnd.open(url);
   }
@@ -391,53 +427,91 @@ class flow {
    *
    * @memberof flow
    */
-  ClickInstructionCategory(){
+  ClickInstructionCategory() {
     let url = this.indexWnd.dataMng.GetInstructionURL();
     this.indexWnd.open(url);
   }
 
   // ここで各画面の遷移
+  ChangeState(nextStateID) {
+    // 現在のステートの後始末
+    switch (this.nowMenuStateID) {
+      case this.MenuStateID.CATEGORY:
+        this.EndCategoryState();
+        break;
+      case this.MenuStateID.COLOR_PICKUP:
+        this.EndColorPickUpState();
+        break;
 
-  // メニューに切り替え
-  ChangeCategoryState(){
+      case this.MenuStateID.COLOR_TYPE:
+        this.EndColorCategoryState();
+        break;
+      case this.MenuStateID.ITEM_PICKUP:
+        this.EndItemPickUpState();
+        break;
+    }
 
-    // メニュー表示設定
-    
+    // 次のステートの用意
+    switch (nextStateID) {
+      case this.MenuStateID.CATEGORY:
+        this.ChangeCategoryState();
+        break;
+      case this.MenuStateID.COLOR_PICKUP:
+        this.ChangeColorPickUpState();
+        break;
 
+      case this.MenuStateID.COLOR_TYPE:
+        this.ChangeColorCategoryState();
+        break;
+      case this.MenuStateID.ITEM_PICKUP:
+        this.ChangeItemPickUpState();
+        break;
+    }
+
+    // 現在のステートIDを更新
+    this.nowMenuStateID = nextStateID;
   }
 
-  EndCategoryState(){
+  // メニューに切り替え
+  ChangeCategoryState() {
+    // メニュー表示設定
+    $(".CategoryList").show();
+  }
 
+  EndCategoryState() {
+    $(".CategoryList").hide();
   }
 
   // カテゴリー選択メニュー
-
   // 機能一覧メニュー
-  ChangeItemPickUpState(){
+  ChangeItemPickUpState() {
+    // 機能一覧を作成
+    this.InitItemPickUp();
 
+    // slickの親を表示状態に変更
+    let slickParent = window.$(".MenuParent");
+    slickParent.show();
   }
 
   // アイテム選択ステート
-  EndItemPickUpState(){
+  EndItemPickUpState() {
+    let slickParent = window.$(".MenuParent");
 
+    // sulick解除
+    slickParent.slick("unslick");
+    // 配下の要素削除
+    slickParent.empty();
+    // 表示状態にする
+    slickParent.hide();
   }
 
   // カラーカテゴリー選択
-  ChangeColorCategoryState(){
+  ChangeColorCategoryState() {}
 
-  }
-
-  EndColorCategoryState(){
-
-  }
+  EndColorCategoryState() {}
 
   // カラー選択
-  ChangeColorPickUpState(){
+  ChangeColorPickUpState() {}
 
-  }
-
-  EndColorPickUpState(){
-
-  }
-
+  EndColorPickUpState() {}
 }
